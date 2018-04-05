99303f14d3a551995522d50c06b477f673f4f521
Blake Larkin <blarkin@g.hmc.edu>
2018-04-04 17:37:21 -0700

    Updated data set generator to only include 2 genres and specific librosa features

--- getFeatures.py
+++ getFeatures.py
@@ -4,13 +4,17 @@ import pandas as pd
 import utils
 
 class DataSetGenerator(object):
-    def __init__(self, subset, data_dir=""):
+    def __init__(self, subset, data_dir="", genre1="Experimental", genre2="Pop", featureSets=['mfcc']):
         """
         Initalize data set generator with the name of the subset we want,
         and load in entire features and tracks datasets. 
 
         :param str subset: Which subset of the tracks we want to use, 'small', 'medium', or 'large'
-        :param str data_dir: Parent directory that stores fma dataset csv's
+        :param str data_dir: Parent directory that stores fma dataset csv's, default=''
+        :param str genre1: First genre to include in X and y, default='Experimental'
+        :param str genre2: Second genre to include in X and y, default='Pop'
+        :param [str] featureSets: sets of high level librosa features to include in X, default=['mfcc']
+                                    set to None for all librosa features
         """
         if subset not in ['small', 'medium', 'large']:
             raise ValueError("size is not 'small', 'medium', or 'large'.")
@@ -18,6 +22,9 @@ class DataSetGenerator(object):
             self.subset = subset
             self.tracks = utils.load(data_dir+'tracks.csv')
             self.features = utils.load(data_dir+'features.csv')
+            self.genre1 = genre1
+            self.genre2 = genre2
+            self.featureSets = featureSets
 
     def create_X_y(self):
         """
@@ -26,9 +33,19 @@ class DataSetGenerator(object):
         indices = self.tracks.index[self.tracks['set', 'subset'] <= self.subset] # grab the track_ids of all songs in the 'self.subset' subset.
         tracks = self.tracks.loc[indices]     # These are subsets of the original tracks
         features = self.features.loc[indices] # and features datasets
-        y = self.__output_classes_from_string_labels(tracks['track', 'genre_top'])
-        X = features.as_matrix()
+
+        genre1 = self.tracks.index[self.tracks['track', 'genre_top'] == self.genre1] # collect tracks of genre1
+        genre2 = self.tracks.index[self.tracks['track', 'genre_top'] == self.genre2] # collect tracks of genre2
+        genreTracks = tracks.loc[indices & (genre1 | genre2)] # get small tracks of wanted genres
+        if self.featureSets is not None:
+            genreFeatures = features.loc[indices & (genre1 | genre2), self.featureSets] # get desired features of small tracks of wanted genres
+        else:
+            genreFeatures = features.loc[indices & (genre1 | genre2)] # use all features
+        X = genreFeatures.as_matrix() # convert features to input matrix
+        y = self.__output_classes_from_string_labels(genreTracks['track', 'genre_top']) # create 1v1 output categorization
+
         return X,y
+
 
     def __output_classes_from_string_labels(self, sequence):
         """
