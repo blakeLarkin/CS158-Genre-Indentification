3ab5c0a659333b1bd020607e86cdcff85175bc4f
floragj <fgallinajones@hmc.edu>
2018-04-05 20:50:15 -0700

    added depth and accuracy graphing

--- graphing.py
+++ graphing.py
@@ -1,7 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from crossValidation import gen_depth_vs_accuracy
 
-def create_multi_line_graph(x_min, x_max, step, ys, title="", x_label="", y_label="", legend=None):
+def create_multi_line_graph(x_min, x_max, ys, step=2, title="", x_label="", y_label="", legend=None):
 	"""
 	Create a line graph with multiple curves, axis labels, and an appropriate legend.
 	:param [float] x
@@ -22,3 +23,21 @@ def create_multi_line_graph(x_min, x_max, step, ys, title="", x_label="", y_labe
 	plt.xlabel(x_label)
 	plt.ylabel(y_label)
 	plt.show()
+
+def gen_depth_vs_acc_plot(genre_prs, min_depth=2, max_depth=2, step=2, feature_sets=None, data_dir=""):
+
+	scores = []
+	legend = []
+	for pair in genre_pairs:
+
+		scores.append([gen_depth_vs_accuracy(genre1=pair[0], genre2=pair[1], max_depth_min=min_depth, max_depth_max=max_depth, step=step, feature_sets=feature_sets, data_dir="" )])
+		legend.append(pair[0] + "and" +  pair[1] )
+
+	x_label="Maximum Decision Tree Depths"
+	y_label = "Accuracy"
+	title="Binary Genre Classification: Decision Trees"
+	create_multi_line_graph(min_depth, max_depth, step, scores, title=title, x_label=x_label, y_label = y_label, legend=legend )
+
+
+
+# data_dir = ../fma_metadata
