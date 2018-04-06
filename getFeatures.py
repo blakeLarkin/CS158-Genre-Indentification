21f95028b885836e42b1f8d9c75dc26987eafc68
Blake Larkin <blarkin@g.hmc.edu>
2018-04-05 20:34:21 -0700

    rough echonest start and genre vs genre pca vizualization

--- getFeatures.py
+++ getFeatures.py
@@ -1,10 +1,12 @@
 import numpy as np
 import pandas as pd
-
+from fixtures import DEF_LIB_SETS, DEF_ECHO_SETS
+import sklearn as skl
+import sklearn.decomposition, sklearn.preprocessing
 import utils
 
 class DataSetGenerator(object):
-    def __init__(self, subset, data_dir="", genre1="Experimental", genre2="Pop", featureSets=['mfcc']):
+    def __init__(self, subset, data_dir="", genre1="Experimental", genre2="Pop", libFeatureSets=DEF_LIB_SETS, echoFeatureSets=DEF_ECHO_SETS):
         """
         Initalize data set generator with the name of the subset we want,
         and load in entire features and tracks datasets. 
@@ -13,20 +15,25 @@ class DataSetGenerator(object):
         :param str data_dir: Parent directory that stores fma dataset csv's, default=''
         :param str genre1: First genre to include in X and y, default='Experimental'
         :param str genre2: Second genre to include in X and y, default='Pop'
-        :param [str] featureSets: sets of high level librosa features to include in X, default=['mfcc']
+        :param [str] libFeatureSets: sets of high level librosa features to include in X, default=['mfcc']
                                     set to None for all librosa features
+        :param [str] echoFeatureSets: sets of high level echonest features to include in X, default=['mfcc']
+                                    set to None for all echonest features
         """
         if subset not in ['small', 'medium', 'large']:
             raise ValueError("size is not 'small', 'medium', or 'large'.")
         else:
             self.subset = subset
             self.tracks = utils.load(data_dir+'tracks.csv')
-            self.features = utils.load(data_dir+'features.csv')
+            self.libFeatures = utils.load(data_dir+'features.csv')
+            self.echoFeatures = utils.load(data_dir+'echonest.csv')
             self.genre1 = genre1
             self.genre2 = genre2
-            self.featureSets = featureSets
+            self.libFeatureSets = libFeatureSets
+            # currently there are issues with echo features, please avoid use
+            self.echoFeatureSets = echoFeatureSets
 
-    def __getSubTracksAndFeatures(self, tracks, subclass, goal, features):
+    def __getSubTracksAndFeatures(self, tracks, subclass, goal, libFeatures, echoFeatures):
         """
         Given a starting list of tracks and features, creates subset of tracks and features that follow a 
         dataset constraint as well as the genre and feature constraints of the generator
@@ -35,21 +42,30 @@ class DataSetGenerator(object):
         :type sequence: Series or DataFrame
         :param str subclass: This is the subcategorization of the track's set, either 'split' or 'subset'
         :param str goal: This is the desired value of the subclass to keep in the subset
-        :param sequence features: a sequence object of starting list of features to create subset from
+        :param sequence libFeatures: a sequence object of starting list of librosa features to create subset from
+        :param sequence echoFeatures: a sequence object of starting list of echonest features to create subset from
         """
         indices = tracks.index[tracks['set', subclass] == goal] # grab the track_ids of all songs in the desired subset.
         subTracks = tracks.loc[indices] # These are subsets of the original tracks
-        subFeatures = features.loc[indices] # and features datasets
+        subLibFeatures = libFeatures.loc[indices] # and librosa features
+        subEchoFeatures = echoFeatures.loc[indices] # and echonest feature datasets
 
         genre1 = tracks.index[tracks['track', 'genre_top'] == self.genre1] # collect tracks of genre1
         genre2 = tracks.index[tracks['track', 'genre_top'] == self.genre2] #  collect tracks of genre2
         
         outTracks = subTracks.loc[indices & (genre1 | genre2)] # get small tracks of wanted genres
 
-        if self.featureSets is not None: # get desired features of small tracks of wanted genres
-            outFeatures = subFeatures.loc[indices & (genre1 | genre2), self.featureSets]
+        if self.libFeatureSets is not None: # get desired librosa features of small tracks of wanted genres
+            libFeatures = subLibFeatures.loc[indices & (genre1 | genre2), self.libFeatureSets]
         else: # use all features
-            outFeatures = subFeatures.loc[indices & (genre1 | genre2)]
+            libFeatures = subLibFeatures.loc[indices & (genre1 | genre2)]
+
+        # currently there are issues with echoFeatures, please avoid use
+        echoFeatures = subEchoFeatures.loc[indices & (genre1 | genre2), ['echonest']]
+        if self.echoFeatureSets is not None: # get desired echonest features of small tracks of wanted genres
+            echoFeatures = echoFeatures.loc[indices & (genre1 | genre2), self.echoFeatureSets]
+
+        outFeatures = pd.concat([libFeatures, echoFeatures]) if len(self.echoFeatureSets) != 0 else libFeatures
 
         return outTracks, outFeatures
 
@@ -58,7 +74,7 @@ class DataSetGenerator(object):
         """
         Create ndarrays from the subsets of features and tracks datasets that we want to look at.
         """
-        genreTracks, genreFeatures = self.__getSubTracksAndFeatures(self.tracks, 'subset', self.subset, self.features) # get desired tracks and features
+        genreTracks, genreFeatures = self.__getSubTracksAndFeatures(self.tracks, 'subset', self.subset, self.libFeatures, self.echoFeatures) # get desired tracks and features
 
         X = genreFeatures.as_matrix() # convert features to input matrix
         y = self.__output_classes_from_string_labels(genreTracks['track', 'genre_top']) # create 1v1 output categorization
@@ -72,16 +88,30 @@ class DataSetGenerator(object):
         """
         indices = self.tracks.index[self.tracks['set', 'subset'] == self.subset] # grab the track_ids of all songs in the 'self.subset' subset.
         tracks = self.tracks.loc[indices]     # These are subsets of the original tracks
-        features = self.features.loc[indices] # and features datasets
+        libFeatures = self.libFeatures.loc[indices] # and librosa features
+        echoFeatures = self.echoFeatures.loc[indices] # and echonest features datasets
 
         splitXy = []
 
         for split in ['training', 'validation', 'test']:
-            subTracks, subFeatures = self.__getSubTracksAndFeatures(tracks, 'split', split, features) # get training items of small set
+            subTracks, subFeatures = self.__getSubTracksAndFeatures(tracks, 'split', split, libFeatures, echoFeatures) # get training items of small set
             splitXy.append(subFeatures.as_matrix()) # append next X
             splitXy.append(self.__output_classes_from_string_labels(subTracks['track', 'genre_top'])) # append next y
 
         return splitXy
+
+    def create_Viz_Data(self, genre1 = None, genre2 = None):
+        if (genre1 is not None) and (genre2 is not None):
+            self.genre1 = genre1
+            self.genre2 = genre2
+
+        genreTracks, genreFeatures = self.__getSubTracksAndFeatures(self.tracks, 'subset', self.subset, self.libFeatures, self.echoFeatures)
+
+        X = skl.decomposition.PCA(n_components=2).fit_transform(genreFeatures)
+        y = genreTracks['track', 'genre_top']
+        y = skl.preprocessing.LabelEncoder().fit_transform(y)
+
+        return X, y
 
     def __output_classes_from_string_labels(self, sequence):
         """
