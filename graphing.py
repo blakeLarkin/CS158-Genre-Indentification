bd38b5759213e1e4c34de94637381ca61303693f
Blake Larkin <blarkin@g.hmc.edu>
2018-04-06 17:10:23 -0700

    moved genre v genre to graphing

--- graphing.py
+++ graphing.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from crossValidation import gen_depth_vs_accuracy
+from getFeatures import DataSetGenerator
+from fixtures import TOP_GENRES
 
 def create_multi_line_graph(x_min, x_max, ys, step=2, title="", x_label="", y_label="", legend=None):
 	"""
@@ -38,6 +40,18 @@ def gen_depth_vs_acc_plot(genre_prs, min_depth=2, max_depth=2, step=2, feature_s
 	title="Binary Genre Classification: Decision Trees"
 	create_multi_line_graph(min_depth, max_depth, step, scores, title=title, x_label=x_label, y_label = y_label, legend=legend )
 
-
+def genrePCA(genres = TOP_GENRES):
+  '''
+  pass in a string list of genre names to see all combinations, default is all genres
+  '''
+  # create DataSetGenerator
+  dsg = DataSetGenerator('small', echoFeatureSets=[])
+  # go through each genre combination
+  for i in range(len(genres)):
+    for j in range(i + 1, len(genres)):
+      X, y = dsg.create_Viz_Data(genres[i], genres[j])
+      plt.scatter(X[:,0], X[:,1], c=y, cmap='RdBu', alpha=0.5)
+      plt.title('PCA Comparison of %s vs. %s' % (genres[i], genres[j]))
+      plt.show()
 
 # data_dir = ../fma_metadata
